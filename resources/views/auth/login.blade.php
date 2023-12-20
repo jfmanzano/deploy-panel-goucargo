88bbc9421f9fb5c26ce4fa3ecb68cecb38f28604
jfmanzano <jfmanzanodev@gmail.com>
2023-12-20 16:36:56 +0100

    feat: change login to get better apparance

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,65 +11,66 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <link rel="icon" href="https://cdn2.globomatik.com/media/favicon/default/faviconglobo23.png" type="image/x-icon">
-    <link rel="stylesheet" href="css/style.css">
+
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
     <!-- Styles -->
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"
-        integrity="sha512-2rNj2KJ+D8s1ceNasTIex6z4HWyOnEYLVC3FigGOmyQCZc2eBXKgOxQmo3oKLHyfcj53uz4QMsRCWNbLd32Q1g=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"
-        integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
 
 </head>
 
-<body class="img js-fullheight"
-    style="background: url(goucargo-fondo.png)  no-repeat center center fixed;  background-size: cover;">
-    <section class="ftco-section ">
-        <div class="container" style="margin-top:22vh;">
-            <div class="row justify-content-center">
-                <div class="col-md-6 text-center mb-5">
-                    <h2 class="heading-section">LOG IN</h2>
-                </div>
-            </div>
-            <div class="row justify-content-center ">
-                <div class="col-md-6 col-lg-4">
-                    <div class="login-wrap p-0">
-                        <form class="signin-form" action="{{ route('login-store') }}" method="POST">
-                            @csrf
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="User" name='user' id="inputEmail"
-                                    required autofocus>
-                            </div>
-                            <div class="form-group">
-                                <input id="password-field" style="background-color: none;" type="password"
-                                    class="form-control" placeholder="Password" name="password" id="inputPassword"
-                                    required>
-                                <span toggle="#password-field"
-                                    class="fa fa-fw fa-eye field-icon toggle-password"></span>
-                            </div>
-                            <div class="form-group">
-                                <button type="submit" class="form-control btn btn-primary submit px-3">LOG IN</button>
-                            </div>
-                            <div class="form-group d-md-flex">
-                                <div class="w-50">
-                                    <label class="checkbox-wrap checkbox-primary">Remember me
-                                        <input type="checkbox" checked>
-                                        <span class="checkmark"></span>
-                                    </label>
+<body>
+
+
+    <section class="pt-5 pb-5 mt-0 align-items-center d-flex bg-dark"
+        style="min-height: 100vh; background-size: cover; background-image: url(goucargo-fondo.png);">
+        <div class="container-fluid">
+
+            <div class="row  justify-content-center align-items-center d-flex-row  h-100">
+                <div class="col-12 col-md-4 col-lg-3   h-50 ">
+                    <div class="card shadow">
+                        <div class="card-body mx-auto">
+
+                            <img src="goucargo-1.png" class="img-fluid rounded" alt="" />
+
+                            <form class="bg-white p-2 text-dark " action="{{ route('login-store') }}" method="POST">
+                                @csrf
+                                <div class="input-group flex-nowrap mb-3">
+                                    <span class="input-group-text" id="addon-wrapping"><i
+                                            class="bi bi-person-circle"></i></span>
+                                    <input type="text" class="form-control" placeholder="Username" name="user"
+                                        aria-label="Username" aria-describedby="addon-wrapping">
                                 </div>
-                            </div>
-                        </form>
+                                <div class="input-group flex-nowrap mb-3">
+                                    <span class="input-group-text" id="addon-wrapping"><i
+                                            class="bi bi-incognito"></i></span>
+                                    <input type="password" class="form-control" placeholder="Password" name="password"
+                                        aria-label="Password" aria-describedby="addon-wrapping">
+                                </div>
+                                <div class="mb-3 form-check">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                    <label class="form-check-label" for="exampleCheck1">Remember me?</label>
+                                </div>
+                                <button type="submit" class="btn btn-dark w-100">Sign in</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
+
+
 </body>
 
 </html>
