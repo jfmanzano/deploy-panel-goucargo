b2895684c75dca9284e105b147873da1f7775af2
jfmanzano <95651605+jfmanzano@users.noreply.github.com>
2023-12-20 11:45:06 +0100

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+		$schedule->command('command:feedordersstatus --force')->everyFiveMinutes();
+		$schedule->command('command:feedproducts --force')->hourly();
+		$schedule->command('command:processorder --force')->everyFiveMinutes();
     }
 
     /**
